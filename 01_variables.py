683488779b553058e1fc498228e59d7e1d10c9cd
Siva <sivakumarpulis@gmail.com>
2025-07-04 13:47:28 +0530

    variables

--- 01_variables.py
+++ 01_variables.py
@@ -2,16 +2,24 @@ a=40
 b=10
 print("Printing value Of A is:", a)
 print("Printing value Of B is:", b)
+
 # This's python program
 d= "This\'s python program"
 print(d)
+
 # This is a List
 c= ["sindu","siva","priya","kumar"]
 print(c, type(c))
+
 # Tuples
 test_tuples = ("java","c++","c#")
 print(test_tuples)
 print(type(test_tuples))
+
 # Dictionary
 test_dict = {"a":1, "b":2, "c":3}
-print(test_dict)
+print(test_dict, type(test_dict))
+
+#set
+test_set={"1","34","sdf"}
+print(test_set, type(test_set))
