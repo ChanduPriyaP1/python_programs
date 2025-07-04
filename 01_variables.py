7731e37510548017d21e6422b2f765dfc9538e0f
Siva <sivakumarpulis@gmail.com>
2025-07-04 13:51:08 +0530

    variables

--- 01_variables.py
+++ 01_variables.py
@@ -1,7 +1,7 @@
-a=40
-b=10
-print("Printing value Of A is:", a)
-print("Printing value Of B is:", b)
+# a=40
+# b=10
+# print("Printing value Of A is:", a)
+# print("Printing value Of B is:", b)
 
 # This's python program
 d= "This\'s python program"
@@ -19,7 +19,15 @@ print(type(test_tuples))
 # Dictionary
 test_dict = {"a":1, "b":2, "c":3}
 print(test_dict, type(test_dict))
-
+# type() function -> prints the datatype of the variable
 #set
 test_set={"1","34","sdf"}
 print(test_set, type(test_set))
+
+a=10
+b=19
+res=a<b
+res_1=a>b
+res_2=a!=b
+res_3=a==b
+print(res res_1 res_2 res_3)
